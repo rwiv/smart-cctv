f1f98819265ef75b3a9ab634b7726d3005c61c0d
rwiv <zc798546@gmail.com>
2024-05-12 07:35:37 +0900

    feat: add hls text code

--- gui/src/dev/pages/TestPage.tsx
+++ gui/src/dev/pages/TestPage.tsx
@@ -1,22 +1,43 @@
-import {useEffect} from "react";
-import {env} from "@/configures/env.ts";
-import {consts} from "@/configures/consts.ts";
+import {useEffect, useRef} from "react";
+import Hls from "hls.js";
 
 export function TestPage() {
 
+  const ref = useRef<HTMLVideoElement>(null);
+
   useEffect(() => {
-    console.log(consts.isDev)
-    console.log(consts.isProd)
-    console.log(env);
+    if (!Hls.isSupported()) {
+      console.log("not supported");
+      return;
+    }
+
+    const hls = new Hls({
+      startLevel: 100,
+      // liveDurationInfinity: true
+    });
+    const liveUrl1 = "https://cph-p2p-msl.akamaized.net/hls/live/2000341/test/master.m3u8";
+    // const m3u8Url = "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8";
+    hls.loadSource(liveUrl1);
+    hls.attachMedia(ref.current as any);
+    hls.on(Hls.Events.MANIFEST_PARSED, async () => {
+      console.log("play");
+      const video = ref.current;
+      if (video === null) return;
+      // refer: https://developer.chrome.com/blog/autoplay?hl=ko#webaudio
+      // 유저의 미디어 참여 지수가 임계값을 넘으면(이전에 재생한 적이 있으면) 자동실행된다
+      // 그렇지 않아 자동실행에 실패했다면 mute 후 자동실행하면 된다
+      try {
+        await video.play();
+      } catch (e) {
+        video.muted = true;
+        await video.play();
+      }
+    });
   }, []);
 
   return (
     <div>
-      <div css={{width: "20rem"}}>
-        {/*<FriendCandidateDev />*/}
-      </div>
-      hello
-      {/*<ParticipatedChatRoomContextMenu />*/}
+      <video ref={ref} controls />
     </div>
   )
 }
