891d98206d6e98fcb2faf2f381d1aaf2ad394ce6
rwiv <zc798546@gmail.com>
2024-05-14 06:24:12 +0900

    refactor: split gui project

--- gui/src/lib/common/array.ts
+++
@@ -1,8 +0,0 @@
-export function reverse<T>(elems: T[] | undefined | null): T[] {
-  if (elems === undefined || elems === null) return [];
-  const result: T[] = [];
-  for (let i = elems.length - 1; i >= 0; i--) {
-    result.push(elems[i]);
-  }
-  return result;
-}
