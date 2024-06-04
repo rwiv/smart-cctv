d220a6f5266ffaaa29170953a3ae11a74691e0d7
rwiv <zc798546@gmail.com>
2024-06-04 12:12:05 +0900

    feat: add address api

---
+++ src/main/kotlin/com/github/smartcctv/address/AddressController.kt
@@ -0,0 +1,35 @@
+package com.github.smartcctv.address
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/rest/addr")
+class AddressController(
+    private val addressStore: AddressStore,
+) {
+
+    @GetMapping("/set/api")
+    fun setApiAddress(@RequestParam address: String): String {
+        addressStore.apiAddress = address
+        return address
+    }
+
+    @GetMapping("/set/cam")
+    fun setCamAddress(@RequestParam address: String): String {
+        addressStore.camAddress = address
+        return address
+    }
+
+    @GetMapping("/get/api")
+    fun getApiAddress(@RequestParam address: String): String {
+        return addressStore.apiAddress
+    }
+
+    @GetMapping("/get/cam")
+    fun getCamAddress(@RequestParam address: String): String {
+        return addressStore.camAddress
+    }
+}
---
+++ src/main/kotlin/com/github/smartcctv/address/AddressStore.kt
@@ -0,0 +1,10 @@
+package com.github.smartcctv.address
+
+import org.springframework.stereotype.Component
+
+@Component
+class AddressStore {
+
+    var apiAddress: String = ""
+    var camAddress: String = ""
+}
